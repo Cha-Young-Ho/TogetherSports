4e0d716a3fd173a53932d97dc1397ea9dd8dbe07
dhkim1522 <dhkim1522@naver.com>
2022-02-14 16:50:23 +0900

    Api-docs backend 작업 디렉터리 추가

---
+++ backend/tos-proejct/src/main/java/com/togethersports/tosproejct/TosProejctApplication.java
@@ -0,0 +1,13 @@
+package com.togethersports.tosproejct;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TosProejctApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TosProejctApplication.class, args);
+	}
+
+}
---
+++ backend/tos-proejct/src/test/java/com/togethersports/tosproejct/TosProejctApplicationTests.java
@@ -0,0 +1,13 @@
+package com.togethersports.tosproejct;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TosProejctApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
