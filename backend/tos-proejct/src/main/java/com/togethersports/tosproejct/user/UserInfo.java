bd8a7493051102e3ff0ce4fc9f8610a1b1eddfcf
dhkim1522 <dhkim1522@naver.com>
2022-02-25 23:39:42 +0900

    회원가입 기능 개발 중입니다.

---
+++ backend/tos-proejct/src/main/java/com/togethersports/tosproejct/user/UserInfo.java
@@ -0,0 +1,50 @@
+package com.togethersports.tosproejct.user;
+
+import com.togethersports.tosproejct.enums.Admin;
+import com.togethersports.tosproejct.enums.Gender;
+import com.togethersports.tosproejct.enums.Provider;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+
+@Builder
+@Data
+@Entity
+@Table(name = "T_USER")
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "USER_SEQUENCE_ID")
+    private int userSequenceId;
+
+    @Column(name = "USER_EMAIL", length = 100, unique = true)
+    private String userEmail;
+
+    @Column(name = "USER_BIRTH", length = 6)
+    private String userBirth;
+
+    @Column(name = "USER_NICKNAME", length = 15)
+    private String userNickname;
+
+    @Column(name = "GENDER", length = 1)
+    @Enumerated(EnumType.STRING)
+    private Gender gender;
+
+    @Column(name = "ADMIN", length = 4)
+    @Enumerated(EnumType.STRING)
+    private Admin admin;
+
+    @Column(name = "USER_NAME", length = 5)
+    private String userName;
+
+    @Column(name = "PROVIDER", length = 20, unique = true)
+    @Enumerated(EnumType.STRING)
+    private Provider provider;
+}
