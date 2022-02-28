9459981a5f1bd4d54be3bb53bed61365f831c998
dhkim1522 <dhkim1522@naver.com>
2022-02-28 14:20:39 +0900

    회원존재유무, 회원가입 기본 기능 개발 완료 (트랜잭션 및 예외처리 보완해야함)

---
+++ backend/tos-proejct/src/main/java/com/togethersports/tosproejct/user/UserLogRepository.java
@@ -0,0 +1,8 @@
+package com.togethersports.tosproejct.user;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+//public interface UserInfoRepository extends JpaRepository<UserInfo, Integer>{
+//}
