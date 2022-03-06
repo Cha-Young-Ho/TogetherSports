c5b7d89d9506a45265a204390cf93351ef5f27e8
이동길 <oio3215@naver.com>
2022-03-06 18:37:38 +0900

    Feature : 간단하게 footer UI 추가

---
+++ frontend/components/footer.js
@@ -0,0 +1,48 @@
+const Footer = () => {
+  return (
+    <>
+      <div className="bg-container">
+        <div className="footer-wrapper">
+          <div className="category">
+            <li>고객센터</li>
+            <li>문의</li>
+            <li>Team</li>
+          </div>
+        </div>
+      </div>
+      <style jsx>{`
+        * {
+          font-family: "NanumBarunGothic";
+          font-weight: bold;
+          font-size: 1.5rem;
+        }
+
+        .bg-container {
+          display: flex;
+          justify-content: center;
+          align-items: center;
+          background-color: rgba(0, 0, 0, 0.16);
+          width: 100%;
+          height: 450px;
+        }
+
+        .footer-wrapper {
+          width: 80%;
+          min-width: 1000px;
+          height: 90%;
+        }
+
+        .category {
+          display: flex;
+        }
+
+        li {
+          padding: 10px;
+          margin-left: 100px;
+        }
+      `}</style>
+    </>
+  );
+};
+
+export default Footer;
