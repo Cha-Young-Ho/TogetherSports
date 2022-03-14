094927022ac07bdf927900372bcf5122b0cddc0a
동길 이 <oio3215@naver.com>
2022-03-15 01:40:37 +0900

    Feature : 마이프로필 API 요청 및 redux에 정보 저장

--- frontend/api/failResponse.js
+++ frontend/api/failResponse.js
@@ -5,6 +5,7 @@ const FailResponse = (codeNumber) => {
   switch (codeNumber) {
     case 1300:
       console.log("토큰이 존재하지 않습니다.");
+      break;
     case 1301:
       console.log("변조된 토큰입니다.");
       postRefreshToken(localStorage.getItem("refreshToken")).then((res) => {
@@ -15,6 +16,7 @@ const FailResponse = (codeNumber) => {
           localStorage.removeItem("refreshToken");
         }
       });
+      break;
     case 1302:
       console.log("만료된 토큰입니다.");
       postRefreshToken(localStorage.getItem("refreshToken")).then((res) => {
@@ -25,8 +27,13 @@ const FailResponse = (codeNumber) => {
           localStorage.removeItem("refreshToken");
         }
       });
+      break;
     case 1303:
       console.log("권한이 없습니다.");
+      break;
+    default:
+      alert("알 수 없는 이유로 실패했습니다.");
+      break;
   }
 };
 
