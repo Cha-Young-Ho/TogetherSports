26db8d633902d3d44aaa1bf704770193b11d0299
Cha-Young-Ho <jkijki1249@gmail.com>
2022-03-16 07:40:08 +0900

    Feature : Room Domain 구현 중#1

--- backend/tos-proejct/src/main/java/com/togethersports/tosproejct/room/Room.java
+++ backend/tos-proejct/src/main/java/com/togethersports/tosproejct/room/Room.java
@@ -1,68 +1,78 @@
-//package com.togethersports.tosproejct.room;
-//
-//import com.fasterxml.jackson.annotation.JsonFormat;
-//import com.togethersports.tosproejct.enums.Tag;
-//import com.togethersports.tosproejct.user.User;
-//
-//import lombok.AllArgsConstructor;
-//import lombok.Builder;
-//import lombok.Getter;
-//import lombok.NoArgsConstructor;
-//
-//import javax.persistence.*;
-//import java.awt.geom.Area;
-//import java.time.LocalDateTime;
-//import java.util.List;
-//
-//@Getter
-//@Builder
-//@Entity
-//@Table(name = "T_ROOM")
-//@NoArgsConstructor
-//@AllArgsConstructor
-//public class Room {
-//
-//    @Id
-//    @GeneratedValue(strategy = GenerationType.AUTO)
-//    private Long roomSequenceId;
-//
-//    @Column(name = "ROOM_TITLE")
-//    private String roomTitle;
-//
-//    @Column(name = "ROOM_CONTENT")
-//    private String roomContent;
-//
-//    @Column(name = "LIMIT_PEOPLE_COUNT")
-//    private int limitPeopleCount;
-//
-//    @Column(name = "PARTICIPANT_COUNT")
-//    private int participantCount;
-//
-//    @Column(name = "EXERCISE")
-//    private String exercise;
-//
-//    @Column(name = "VIEW_COUNT")
-//    private int viewCount;
-//
-////    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm", timezone = "Asia/Seoul")
-////    @Column(name = "APPOINTMENT_DATE")
-////    private LocalDateTime appointmentDate;
-//
-////    @Column(name = "AREA")
-////    private ActiveAreas activeAreas;
-//
-////    private List<RoomImage> roomImageList;
-//
-////    private List<Tag> tagList;
-//
-//    @ManyToOne
-//    @JoinColumn(name = "")
-//    private User host;
-//
-//    private List<User> participantList;
-//
-//
-//
-//
-//
-//}
+package com.togethersports.tosproejct.room;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.togethersports.tosproejct.room.fields.RoomArea;
+import com.togethersports.tosproejct.room.fields.RoomImage;
+import com.togethersports.tosproejct.room.fields.RoomTag;
+import com.togethersports.tosproejct.user.User;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Getter
+@Builder
+@Entity
+@Table(name = "T_ROOM")
+@NoArgsConstructor
+@AllArgsConstructor
+public class Room {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "ROOM_SEQUENCE_ID")
+    private Long roomSequenceId;
+
+    @Column(name = "ROOM_TITLE")
+    private String roomTitle;
+
+    @Column(name = "ROOM_CONTENT")
+    private String roomContent;
+
+    @Column(name = "LIMIT_PEOPLE_COUNT")
+    private int limitPeopleCount;
+
+    @Column(name = "PARTICIPANT_COUNT")
+    private int participantCount;
+
+    @Column(name = "EXERCISE")
+    private String exercise;
+
+    @Column(name = "VIEW_COUNT")
+    private int viewCount;
+
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm", timezone = "Asia/Seoul")
+    @Column(name = "START_APPOINTMENT_DATE")
+    private LocalDateTime startAppointmentDate;
+
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm", timezone = "Asia/Seoul")
+    @Column(name = "END_APPOINTMENT_DATE")
+    private LocalDateTime endAppointmentDate;
+
+    @OneToOne(mappedBy = "room")
+    private RoomArea roomArea;
+
+    @OneToMany(mappedBy = "room")
+    private List<RoomImage> roomImageList;
+
+
+    @OneToMany(mappedBy = "room")
+    private List<RoomTag> roomTagList;
+
+    @ManyToOne
+    @JoinColumn(name = "USER_SEQUENCE_ID")
+    private User host;
+
+
+    //private List<User> participantList;
+
+
+
+
+
+}
