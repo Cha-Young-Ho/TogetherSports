9bcf4f2915cb7f436bcf6cbc8ec1ca65e8a86b04
Cha-Young-Ho <jkijki1249@gmail.com>
2022-03-17 05:52:00 -0700

    Feature : 방 이미지 기본 값 설정 완료

--- backend/tos-proejct/src/main/java/com/togethersports/tosproejct/room/repository/RoomImageRepository.java
+++ backend/tos-proejct/src/main/java/com/togethersports/tosproejct/room/repository/RoomImageRepository.java
@@ -1,7 +1,7 @@
-//package com.togethersports.tosproejct.room.repository;
-//
-//import com.togethersports.tosproejct.room.fields.RoomImage;
-//import org.springframework.data.jpa.repository.JpaRepository;
-//
-//public interface RoomImageRepository extends JpaRepository<RoomImage, Long> {
-//}
+package com.togethersports.tosproejct.room.repository;
+
+import com.togethersports.tosproejct.room.fields.RoomImage;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RoomImageRepository extends JpaRepository<RoomImage, Long> {
+}
--- backend/tos-proejct/src/main/java/com/togethersports/tosproejct/room/repository/RoomTagRepository.java
+++ backend/tos-proejct/src/main/java/com/togethersports/tosproejct/room/repository/RoomTagRepository.java
@@ -1,7 +1,7 @@
-//package com.togethersports.tosproejct.room.repository;
-//
-//import com.togethersports.tosproejct.room.fields.RoomTag;
-//import org.springframework.data.jpa.repository.JpaRepository;
-//
-//public interface RoomTagRepository extends JpaRepository<RoomTag, Long> {
-//}
+package com.togethersports.tosproejct.room.repository;
+
+import com.togethersports.tosproejct.room.fields.RoomTag;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RoomTagRepository extends JpaRepository<RoomTag, Long> {
+}
