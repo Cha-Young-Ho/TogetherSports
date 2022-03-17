72a3756133dc07619a64d1196b28e010af8e76de
Cha-Young-Ho <jkijki1249@gmail.com>
2022-03-17 05:23:38 -0700

    BugFix : room 도메인 DB 대소문자 해결

--- backend/tos-proejct/src/main/java/com/togethersports/tosproejct/room/Room.java
+++ backend/tos-proejct/src/main/java/com/togethersports/tosproejct/room/Room.java
@@ -1,80 +1,79 @@
-//package com.togethersports.tosproejct.room;
-//
-//import com.fasterxml.jackson.annotation.JsonFormat;
-//import com.togethersports.tosproejct.room.fields.RoomArea;
-//import com.togethersports.tosproejct.room.fields.RoomImage;
-//import com.togethersports.tosproejct.room.fields.RoomTag;
-//import com.togethersports.tosproejct.user.User;
-//
-//import lombok.AllArgsConstructor;
-//import lombok.Builder;
-//import lombok.Getter;
-//import lombok.NoArgsConstructor;
-//import org.hibernate.annotations.DynamicInsert;
-//
-//import javax.persistence.*;
-//import java.time.LocalDateTime;
-//import java.util.List;
-//
-//@Getter
-//@Builder
-//@Entity
-//@Table(name = "T_ROOM")
-//@NoArgsConstructor
-//@AllArgsConstructor
-//@DynamicInsert
-//public class Room {
-//
-//    @Id
-//    @GeneratedValue(strategy = GenerationType.AUTO)
-//    @Column(name = "ROOM_SEQUENCE_ID")
-//    private Long roomSequenceId;
-//
-//    @Column(name = "ROOM_TITLE")
-//    private String roomTitle;
-//
-//    @Column(name = "ROOM_CONTENT")
-//    private String roomContent;
-//
-//    @Column(name = "LIMIT_PEOPLE_COUNT")
-//    private int limitPeopleCount;
-//
-//    @Column(name = "PARTICIPANT_COUNT", columnDefinition = "integer default 1")
-//    private int participantCount;
-//
-//    @Column(name = "EXERCISE")
-//    private String exercise;
-//
-//    @Column(name = "VIEW_COUNT", columnDefinition = "integer default 1")
-//    private int viewCount;
-//
-//    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm", timezone = "Asia/Seoul")
-//    @Column(name = "START_APPOINTMENT_DATE")
-//    private LocalDateTime startAppointmentDate;
-//
-//    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm", timezone = "Asia/Seoul")
-//    @Column(name = "END_APPOINTMENT_DATE")
-//    private LocalDateTime endAppointmentDate;
-//
-//    @OneToOne(mappedBy = "room")
-//    private RoomArea roomArea;
-//
-//    @OneToMany(mappedBy = "room")
-//    private List<RoomImage> roomImages;
-//
-//
-//    @OneToMany(mappedBy = "room")
-//    private List<RoomTag> roomTagList;
-//
-//    @ManyToOne
-//    @JoinColumn(name = "USER_SEQUENCE_ID")
-//    private User host;
-//
-//
-//    //private List<User> participantList;
-//
-//
-//
-//
-//
-//}
+package com.togethersports.tosproejct.room;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.togethersports.tosproejct.room.fields.RoomArea;
+import com.togethersports.tosproejct.room.fields.RoomImage;
+import com.togethersports.tosproejct.room.fields.RoomTag;
+import com.togethersports.tosproejct.user.User;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.DynamicInsert;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Getter
+@Builder
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@DynamicInsert
+@Table
+public class Room {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long roomSequenceId;
+
+    @Column
+    private String roomTitle;
+
+    @Column
+    private String roomContent;
+
+    @Column
+    private int limitPeopleCount;
+
+    @Column(columnDefinition = "integer default 1")
+    private int participantCount;
+
+    @Column
+    private String exercise;
+
+    @Column(columnDefinition = "integer default 1")
+    private int viewCount;
+
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm", timezone = "Asia/Seoul")
+    @Column
+    private LocalDateTime startAppointmentDate;
+
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm", timezone = "Asia/Seoul")
+    @Column
+    private LocalDateTime endAppointmentDate;
+
+    @OneToOne(mappedBy = "room")
+    private RoomArea roomArea;
+
+    @OneToMany(mappedBy = "room")
+    private List<RoomImage> roomImages;
+
+
+    @OneToMany(mappedBy = "room")
+    private List<RoomTag> roomTagList;
+
+    @ManyToOne
+    @JoinColumn(name = "userSequenceId")
+    private User host;
+
+
+    //private List<User> participantList;
+
+
+
+
+
+}
--- backend/tos-proejct/src/main/java/com/togethersports/tosproejct/room/fields/RoomImage.java
+++ backend/tos-proejct/src/main/java/com/togethersports/tosproejct/room/fields/RoomImage.java
@@ -1,46 +1,45 @@
-//package com.togethersports.tosproejct.room.fields;
-//
-//import com.togethersports.tosproejct.room.Room;
-//import lombok.AllArgsConstructor;
-//import lombok.Builder;
-//import lombok.Getter;
-//import lombok.RequiredArgsConstructor;
-//import org.hibernate.annotations.DynamicInsert;
-//
-//import javax.persistence.*;
-//
-//@Getter
-//@AllArgsConstructor
-//@RequiredArgsConstructor
-//@Builder
-//@Entity
-//@Table(name = "T_ROOM_IMAGE")
-//@DynamicInsert
-//public class RoomImage {
-//
-//    @Id
-//    @GeneratedValue(strategy = GenerationType.AUTO)
-//    private Long roomImageSequenceId;
-//
-//    @ManyToOne
-//    @JoinColumn(name = "ROOM_SEQUENCE_ID")
-//    private Room room;
-//
-//    @Column(name = "ROOM_IMAGE_PATH", columnDefinition = "varchar(255) default '/Users/younghocha/files/profile/img/TogetherSports_Default_Image.png''")
-//    private String roomImagePath;
-//
-//    @Column(name = "ROOM_IMAGE_REAL_NAME", columnDefinition = "varchar(255) default 'Together_Sports_Image'")
-//    private String roomImageRealName;
-//
-//    @Column(name = "ROOM_IMAGE_SAVE_NAME", columnDefinition = "varchar(255) default 'Default_Image'")
-//    private String roomImageSaveName;
-//
-//    @Column(name = "ROOM_IMAGE_EXTENSION", columnDefinition = "varchar(255) default 'png'")
-//    private String roomImageExtension;
-//
-//
-//    public void updateRoom(Room room){
-//        this.room = room;
-//    }
-//
-//}
+package com.togethersports.tosproejct.room.fields;
+
+import com.togethersports.tosproejct.room.Room;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.hibernate.annotations.DynamicInsert;
+
+import javax.persistence.*;
+
+@Getter
+@AllArgsConstructor
+@RequiredArgsConstructor
+@Builder
+@Entity
+@Table
+public class RoomImage {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long roomImageSequenceId;
+
+    @ManyToOne
+    @JoinColumn(name = "roomSequenceId")
+    private Room room;
+
+    @Column
+    private String roomImagePath;
+
+    @Column
+    private String roomImageRealName;
+
+    @Column
+    private String roomImageSaveName;
+
+    @Column
+    private String roomImageExtension;
+
+
+    public void updateRoom(Room room){
+        this.room = room;
+    }
+
+}
