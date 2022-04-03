78976faf9b974a8f9b01efe2660b98b9b90a8503
sagongjieun <gonii0821@naver.com>
2022-04-03 21:38:55 +0900

    Feature : 이미지 대표사진 기능 구현

--- frontend/components/rooms/setRoomImages.js
+++ frontend/components/rooms/setRoomImages.js
@@ -1,14 +1,13 @@
 import { useEffect, useState } from "react";
 
-/* 수정 필요 */
-// 1. 대표사진에 대한 구현
-
 const SetRoomImages = (props) => {
-  const [image, setImage] = useState("");
-  const [imagesrc, setImagesrc] = useState("");
+  const [inputImageName, setInputImageName] = useState("");
+  const [imageSrc, setImageSrc] = useState("");
   const [roomImage, setRoomImage] = useState([]); //서버로 보낼 데이터
   const [imagePreview, setImagePreview] = useState([]);
+  const [representativeIndex, setRepresentativeIndex] = useState(0);
 
+  // 상위 컴포넌트로 이미지 전달
   useEffect(() => {
     if (props.getData) {
       props.getData(roomImage);
@@ -26,7 +25,7 @@ const SetRoomImages = (props) => {
       reader.onload = () => {
         baseURL = reader.result;
         const sliceIndex = baseURL.indexOf(",");
-        setImagesrc((imagesrc = baseURL.substr(sliceIndex + 1)));
+        setImageSrc((imageSrc = baseURL.substr(sliceIndex + 1)));
         setImagePreview((prev) => [...prev, (imagePreview = baseURL)]);
         resolve(baseURL);
       };
@@ -43,13 +42,13 @@ const SetRoomImages = (props) => {
     const imageFileExtension = file.type.split("/")[1];
 
     if (roomImage.length < 5) {
-      setImage(file.name);
+      setInputImageName(file.name);
       encodeFileToBase64(file).then(() => {
         setRoomImage([
           ...roomImage,
           {
             roomImageExtension: imageFileExtension,
-            imageSource: imagesrc,
+            imageSource: imageSrc,
           },
         ]);
       });
@@ -66,7 +65,7 @@ const SetRoomImages = (props) => {
     const selectedImg = document.querySelector(`.img-${imageIndex}`);
 
     // input 비우기
-    setImage((image = ""));
+    setInputImageName((inputImageName = ""));
 
     // 프리뷰에서 삭제
     setImagePreview((prev) =>
@@ -85,25 +84,23 @@ const SetRoomImages = (props) => {
     );
   };
 
-  const onChangeRepresentativeImage = (e) => {
-    const targetIndex = e.target.classList[1].slice(-1);
-    const moveValue = Math.abs(targetIndex) * -1;
-    console.log(changeArrayOrder(imagePreview, targetIndex, moveValue));
-  };
-
   // 대표사진을 위해 배열의 index 변경
   const changeArrayOrder = (arr, targetIndex, moveValue) => {
     const newPositionIndex = targetIndex + moveValue; // 이동할 index
     if (newPositionIndex < 0 || newPositionIndex >= arr.length) return;
-    // const tempArr = JSON.parse(JSON.stringify(arr));
-    const tempArr = arr;
+    const tempArr = JSON.parse(JSON.stringify(arr));
     const target = tempArr.splice(targetIndex, 1)[0];
     tempArr.splice(newPositionIndex, 0, target);
-    return tempArr;
+    setRoomImage((roomImage = tempArr));
   };
 
-  const onClickImg = () => {
-    console.log("이미지누름");
+  const onChangeRepresentativeImage = (e) => {
+    const targetIndex = e.target.classList[1].slice(-1);
+    const moveValue = Math.abs(targetIndex) * -1;
+
+    setInputImageName((inputImageName = "")); // input 비우기
+    setRepresentativeIndex((representativeIndex = Number(targetIndex))); // 대표사진 바꾸기
+    changeArrayOrder(roomImage, targetIndex, moveValue);
   };
 
   return (
@@ -111,7 +108,7 @@ const SetRoomImages = (props) => {
       <div className="content-images">
         <div className="images">
           <p>사진추가</p>
-          <input readOnly className="image-name" value={image} />
+          <input readOnly className="image-name" value={inputImageName} />
           <label htmlFor="filename">
             <div>파일찾기</div>
           </label>
@@ -123,29 +120,39 @@ const SetRoomImages = (props) => {
           />
         </div>
 
+        <p>* 선택된 사진이 대표사진이 됩니다!</p>
+
         <div className="previews">
           {imagePreview.map((preview, index) => {
-            return (
-              <div className="preview" key={index}>
-                <div
-                  className={`preview-${index}`}
-                  onClick={onChangeRepresentativeImage}
-                  style={{
-                    border: index === 0 ? "solid 3px #468f5b" : "none",
-                  }}
-                >
-                  <img
-                    src={preview}
-                    alt="preview"
-                    className={`img-${index}`}
-                    onClick={onClickImg}
-                  />
+            if (index === representativeIndex) {
+              return (
+                <div className="preview" key={index}>
+                  <div
+                    className={`preview-${index} preview-clicked`}
+                    onClick={onChangeRepresentativeImage}
+                  >
+                    <img src={preview} className={`img-${index}`} />
+                  </div>
+                  <button className={`btn-${index}`} onClick={deleteImage}>
+                    X
+                  </button>
                 </div>
-                <button className={`btn-${index}`} onClick={deleteImage}>
-                  X
-                </button>
-              </div>
-            );
+              );
+            } else {
+              return (
+                <div className="preview" key={index}>
+                  <div
+                    className={`preview-${index} preview-unclicked`}
+                    onClick={onChangeRepresentativeImage}
+                  >
+                    <img src={preview} className={`img-${index}`} />
+                  </div>
+                  <button className={`btn-${index}`} onClick={deleteImage}>
+                    X
+                  </button>
+                </div>
+              );
+            }
           })}
         </div>
       </div>
@@ -161,7 +168,7 @@ const SetRoomImages = (props) => {
           width: 550px;
           height: 40px;
           padding: 5px 10px 5px 14px;
-          margin-bottom: 20px;
+          margin-bottom: 10px;
           border: solid 1px #e8e8e8;
           border-radius: 10px;
           display: flex;
@@ -182,7 +189,7 @@ const SetRoomImages = (props) => {
           padding: 5px;
         }
 
-        .content-images label {
+        .images label {
           width: 70px;
           height: 28px;
           background-color: #08555f;
@@ -197,13 +204,19 @@ const SetRoomImages = (props) => {
           justify-content: center;
         }
 
-        .content-images input[type="file"] {
+        .images input[type="file"] {
           position: absolute;
           width: 0;
           height: 0;
           padding: 0;
           border: 0;
           overflow: hidden;
+        }
+
+        .content-images > p {
+          margin-bottom: 20px;
+          font-size: 1.2em;
+          color: #b5b5b5;
         }
 
         .previews {
@@ -214,14 +227,29 @@ const SetRoomImages = (props) => {
         }
 
         .preview {
+          height: 100px;
           display: table;
           text-align: center;
         }
 
-        .preview div {
-          width: 90px;
+        .preview-unclicked {
+          width: 97px;
           height: 90px;
           margin: 0 10px;
+          border: none;
+          border-radius: 10px;
+          background-color: #d8d8d8;
+          overflow: hidden;
+          display: table-cell;
+          vertical-align: middle;
+          cursor: pointer;
+        }
+
+        .preview-clicked {
+          width: 97px;
+          height: 90px;
+          margin: 0 10px;
+          border: solid 3px #468f5b;
           border-radius: 10px;
           background-color: #d8d8d8;
           overflow: hidden;
@@ -244,7 +272,7 @@ const SetRoomImages = (props) => {
           border: none;
           border-radius: 50%;
           opacity: 0.8;
-          background-color: #f2f2f2;
+          background-color: darkgray;
           font-weight: bold;
           font-size: 1.2em;
           color: white;
