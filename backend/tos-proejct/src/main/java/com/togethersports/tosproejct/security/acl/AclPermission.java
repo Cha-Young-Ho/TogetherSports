938c45b384506eba09815bebe3684979a8f5af5d
seunjeon <jeonseun1234@gmail.com>
2022-04-13 13:14:22 +0900

    Feature : acl 권한 위임 기능 구현
    - acl 권한 위임 시 소유권 이전 및 권한 변경 기능 구현
    - acl 정보 생성 어노테이션 및 aop 수정

---
+++ backend/tos-proejct/src/main/java/com/togethersports/tosproejct/security/acl/AclPermission.java
@@ -0,0 +1,32 @@
+package com.togethersports.tosproejct.security.acl;
+
+import org.springframework.security.acls.domain.BasePermission;
+import org.springframework.security.acls.model.Permission;
+
+import java.util.List;
+
+/**
+ * @author seunjeon
+ */
+public enum AclPermission {
+    // 도메인 객체 관리자 권한 모든 오퍼레이션 허용
+    ADMIN(List.of(BasePermission.READ,
+            BasePermission.CREATE,
+            BasePermission.WRITE,
+            BasePermission.DELETE,
+            BasePermission.ADMINISTRATION)),
+
+    // 도메인 객체 매니저 권한 삭제 및 관리자 권한 제외 모든 오퍼레이션 허용
+    MANAGER(List.of(BasePermission.READ,
+            BasePermission.CREATE,
+            BasePermission.WRITE));
+    private final List<Permission> permission;
+
+    AclPermission(List<Permission> permission) {
+        this.permission = permission;
+    }
+
+    public List<Permission> getPermission() {
+        return permission;
+    }
+}
