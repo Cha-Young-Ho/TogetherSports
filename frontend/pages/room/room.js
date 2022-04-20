79d755308c9306473cc690625945d8c2f7627ea6
sagongjieun <gonii0821@naver.com>
2022-04-20 15:39:03 +0900

    Refactoring : 방 리팩토링

--- frontend/pages/room/room.js
+++ frontend/pages/room/room.js
@@ -6,6 +6,7 @@ import ParticipantList from "../../components/rooms/participantList";
 /* 수정 필요 */
 // 1. 명세 후에 제대로 다시 하기
 // 2. 조회수에 대한 디자인 필요
+// 3. 방장의 경우에만 방 수정하기 버튼이 보이게 하기 -> 방 수정하기 모달 뜨게
 
 const Room = () => {
   const [tag, setTag] = useState([
@@ -31,9 +32,9 @@ const Room = () => {
     useState("2022-04-01T14:30");
   const [limitPeopleCount, setLimitPeopleCount] = useState("30명");
   const [participantCount, setParticipantCount] = useState("10명");
-  const [exercise, setExercise] = useState("축구");
+  const [exercise, setExercise] = useState("배드민턴");
   const [roomContent, setRoomContent] = useState("");
-  const [area, setArea] = useState("");
+  const [area, setArea] = useState("서울 송파구 올림픽로 19-2");
 
   useEffect(() => {
     const mapScript = document.createElement("script");
@@ -185,7 +186,7 @@ const Room = () => {
         <div className="location-info">
           <div>
             <p>위치 정보</p>
-            <p>서울 강동구 올림픽로 223-1</p>
+            <p>{area}</p>
           </div>
           <div className="long-line"></div>
           <div id="map"></div>
@@ -463,7 +464,8 @@ const Room = () => {
           background-color: #f4f4f4;
           resize: none;
           font-size: 1.4em;
-          overflow: scroll;
+          scroll: auto;
+          overflow-x: hidden;
         }
 
         .location-info {
