8d07e35a8cc4f0c9f6cd0f5613030e97ee849eb9
seunjeon <jeonseun1234@gmail.com>
2022-05-02 17:19:43 +0900

    Feature: 엑셀 파일 읽기 기능 구현
    - apache poi 라이브러리 추가
    - Excel.java 엑셀 파일 클래스 정의
    - ExcelReader.java 엑셀 시트별 Reader 클래스 정의

--- backend/tos-proejct/src/main/java/com/togethersports/tosproejct/common/file/util/ResourceFileLoader.java
+++ backend/tos-proejct/src/main/java/com/togethersports/tosproejct/common/file/util/ResourceFileLoader.java
@@ -1,6 +1,7 @@
 package com.togethersports.tosproejct.common.file.util;
 
 import com.togethersports.tosproejct.common.file.exception.ResourceFileNotFoundException;
+
 import lombok.Getter;
 
 import java.io.File;
@@ -10,13 +11,14 @@ import java.util.Objects;
 /**
  * <h1>ResourceFileLoader</h1>
  * <p>
- *     리소스 경로의 파일을 읽어오는 역할을 담당한다.
+ * 리소스 경로의 파일을 읽어오는 역할을 담당한다.
  * </p>
+ *
  * @author seunjeon
  */
-@Getter
 public class ResourceFileLoader {
 
+    @Getter
     private File file;
 
     public ResourceFileLoader(String fileName) {
