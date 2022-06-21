4543caa43d6b1c39871bc071cd55c3494daba2ad
seunjeon <jeonseun1234@gmail.com>
2022-06-21 16:43:51 +0900

    feat: 파일 삭제, 불러오기 기능 구현
    - StorageService 인터페이스 기능 추가
    - 특정 경로내 파일 삭제 기능 구현
    - 특정 경로내 파일 불러오기 기능 구현 (base64 인코딩 문자열 반환)

--- backend/tos-project/src/main/java/com/togethersports/tosproject/TosProjectApplication.java
+++ backend/tos-project/src/main/java/com/togethersports/tosproject/TosProjectApplication.java
@@ -1,11 +1,8 @@
 package com.togethersports.tosproject;
 
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
-import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @EnableJpaAuditing
