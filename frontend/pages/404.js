5ca496c06b7c389130f0b1314ec463b42454a5de
sagongjieun <gonii0821@naver.com>
2022-06-25 19:02:20 +0900

    Feature : 에러페이지 생성

---
+++ frontend/pages/404.js
@@ -0,0 +1,5 @@
+const NotFound = () => {
+  return <p>404 에러입니다.</p>;
+};
+
+export default NotFound;
---
+++ frontend/pages/_error.js
@@ -0,0 +1,5 @@
+const Error = () => {
+  return <p>500번 에러입니다.</p>;
+};
+
+export default Error;
