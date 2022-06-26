a12c4405d5cc1be92e5de82c4d14e3d58a36fbd4
sagongjieun <gonii0821@naver.com>
2022-06-26 18:09:01 +0900

    Feature : 에러페이지 커스텀

--- frontend/pages/404.js
+++ frontend/pages/404.js
@@ -1,5 +1,24 @@
 const NotFound = () => {
-  return <p>404 에러입니다.</p>;
+  return (
+    <>
+      <div className="content-wrapper">
+        <p>404 NOT FOUND</p>
+        <p>해당 페이지를 찾을 수 없습니다.</p>
+      </div>
+
+      <style jsx>{`
+        .content-wrapper {
+          display: flex;
+          flex-direction: column;
+          align-items: center;
+        }
+
+        p {
+          font-size: 6rem;
+        }
+      `}</style>
+    </>
+  );
 };
 
 export default NotFound;
--- frontend/pages/_error.js
+++ frontend/pages/_error.js
@@ -1,5 +1,23 @@
 const Error = () => {
-  return <p>500번 에러입니다.</p>;
+  return (
+    <>
+      <div className="content-wrapper">
+        <p>에러가 발생했습니다.</p>
+      </div>
+
+      <style jsx>{`
+        .content-wrapper {
+          display: flex;
+          flex-direction: column;
+          align-items: center;
+        }
+
+        p {
+          font-size: 6rem;
+        }
+      `}</style>
+    </>
+  );
 };
 
 export default Error;
