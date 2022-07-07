76b43e219153829e182d4181a793f2325ec73820
MoveRoad <oio3215@naver.com>
2022-07-07 21:36:05 +0900

    Fix : 파비콘 추가

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -2,10 +2,15 @@ import React from "react";
 import "../styles/globals.css";
 import NavigationBar from "../components/navigationBar";
 import { wrapper } from "../store/rootReducer";
+import Head from "next/head";
 
 function MyApp({ Component }) {
   return (
     <>
+      <Head>
+        <link rel="shortcut icon" href="/favicon_16x16.ico" />
+        <link rel="icon" href="/favicon_16x16.ico" />
+      </Head>
       <NavigationBar />
       <Component />
     </>
